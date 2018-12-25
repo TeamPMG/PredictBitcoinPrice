74504b664f40f712bc3a4770c9693b86650c9f51
mirukuma <m.love.kmoy@gmail.com>
2018-12-25 14:19:26 +0900

    keras → chainer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # PredictBitcoinPrice
 This project tries to predict the price of Bitcoin by using DeepLearning.
+
+#training data
+https://jpbitcoin.com/markets_calc
--- main.py
+++ main.py
@@ -2,46 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import csv
 
-from keras.models import Sequential
-from keras.layers import Dense,Dropout
-from keras.layers.core import Activation,Flatten
-from keras.layers.convolutional import Conv1D
-from keras.layers.normalization import BatchNormalization
-from keras.layers import Reshape
-from keras.optimizers import Adam
-BATCH_SIZE = 10
+import chainer
+from chainer import Chain, Variable
+import chainer.functions as F
+import chainer.links as L
+from chainer import serializers
+
+BATCH_SIZE = 256
 NUM_EPOCH = 10000
 INPUT_SIZE=128
 OUTPUT_SIZE=16
-TESTDATA_SIZE=200
+TESTDATA_SIZE = 200
 
-FILE_NAME="jpbitcoinbpi201801.csv"
+FILE_NAME="jpbitcoinbpi201811.csv"
 
-def predict_model():
-    model = Sequential()
-    model.add(Dense(input_dim=128, output_dim=128))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.25))
-    model.add(Reshape((128,1), input_shape=(128,)))
-    model.add(Conv1D(256, 4,strides=1, padding='same'))
-    model.add(Activation('relu'))
-    model.add(Conv1D(256, 4,strides=1, padding='same'))
-    model.add(BatchNormalization())
-    model.add(Activation('relu'))
-    model.add(Conv1D(256, 4,strides=2, padding='same'))#64
-    model.add(Activation('relu'))
-    model.add(Conv1D(256, 4,strides=2, padding='same'))#32
-    model.add(Activation('relu'))
-    model.add(Conv1D(256, 4,strides=2, padding='same'))#16
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Conv1D(1, 1,strides=1, padding='same'))#16
-    model.add(Activation('linear'))
-    model.add(Reshape((16,), input_shape=(16,1)))
-    
-    
-    
-    return model
+class predict_model(chainer.Chain):
+    def __init__(self, in_dim, out_dim, hidden_dim):
+        super(predict_model,self).__init__(
+            l1 = L.Linear(in_dim, in_dim),
+            l2 = L.Linear(in_dim, out_dim),
+            c1 = L.ConvolutionND(ndim=1, in_channels=1, out_channels=hidden_dim, ksize=3, pad=1),
+            c2 = L.ConvolutionND(ndim=1, in_channels=hidden_dim, out_channels=hidden_dim, ksize=3, pad=1),
+            c3 = L.ConvolutionND(ndim=1, in_channels=hidden_dim, out_channels=1, ksize=3, pad=1),
+        )
+
+    def __call__(self,x):
+        h = F.relu(self.l1(x))
+        h = F.reshape(h, (h.shape[0],1,h.shape[1]))
+        h = F.relu(self.c1(h))
+        h = F.dropout(h, 0.5)
+        h = F.relu(self.c2(h))
+        h = F.dropout(h, 0.5)
+        h = F.relu(self.c3(h))
+        h = F.dropout(h, 0.5)
+        return self.l2(h)
 
 def data_import():
     
@@ -53,73 +47,76 @@ def data_import():
             #date,open,high,low,close,volume
         #x = [x[0] for x in data]
         data=data[1:]
-        
         x = [i for i in range(len(data))]
-        y = [y[4] for y in data]
-        plt.plot(x,y)
-        plt.show()
-    return y
+        y = [float(y[4]) for y in data]
+        #plt.plot(x,y)
+        #plt.show()
+        return y
 
 
 def train():
+    print('import data....')
     data = data_import()
-    X=[]
-    y=[]
-    for i in range(len(data)-(INPUT_SIZE+OUTPUT_SIZE+TESTDATA_SIZE)):
-       X.append(data[i:i+INPUT_SIZE])
-       y.append(data[i+INPUT_SIZE:i+INPUT_SIZE+OUTPUT_SIZE])
-    X=np.array(X).astype("float32")
-    y=np.array(y).astype("float32")
-    data=np.array(data).astype("float32")
-    test_x=data[1800:1928].reshape(1,128)
-    test_y=data[1928:1944].reshape(1,16)
-    mean=test_x.mean()
-    std=test_x.std()
-    test_x=(test_x-mean)/std
-    test_y=(test_y-mean)/std
-        
+    print('success!')
+    data_x=[]
+    data_y=[]
+    for i in range(len(data) - INPUT_SIZE - OUTPUT_SIZE):
+       data_x.append(data[i:i+INPUT_SIZE])
+       data_y.append(data[i+INPUT_SIZE:i+INPUT_SIZE+OUTPUT_SIZE])
 
-    
-    predict=predict_model()
+    data_x=np.array(data_x).astype("float32")
+    data_y=np.array(data_y).astype("float32")
+
+    X = data_x[0:len(data_x) - TESTDATA_SIZE]
+    y = data_y[0:len(data_y) - TESTDATA_SIZE]
+    test_x = data_x[len(data_x) - TESTDATA_SIZE:]
+    test_y = data_y[len(data_y) - TESTDATA_SIZE:]
+
+    mean=test_x.mean(axis=1)
+    std=test_x.std(axis=1)
+    test_x=(test_x-mean.reshape(TESTDATA_SIZE,1))/std.reshape(TESTDATA_SIZE,1)
+    test_y=(test_y-mean.reshape(TESTDATA_SIZE,1))/std.reshape(TESTDATA_SIZE,1)
+    model = predict_model(INPUT_SIZE, OUTPUT_SIZE,32)
     try:
-        predict.load_weights('predict.h5')
+        serializers.load_npz("predict.model", model)
         print("loaded")
     except:
         print("couldn't load")
-    opt = Adam(lr=1e-4, beta_1=0.5)
-    predict.compile(loss='mean_squared_error', optimizer=opt)
-    
-    
+
+    opt = chainer.optimizers.SGD(0.01)
+    opt.setup(model)  
     num_batches = int(X.shape[0] / BATCH_SIZE)
     for epoch in range(NUM_EPOCH):
         perm = np.random.permutation(X.shape[0])
         for index in range(num_batches):
             X_batch = X[perm[index*BATCH_SIZE:(index+1)*BATCH_SIZE]]
-            y_batch = y[perm[index*BATCH_SIZE:(index+1)*BATCH_SIZE]]
+            Y_batch = y[perm[index*BATCH_SIZE:(index+1)*BATCH_SIZE]]
             #return X_batch
             mean = X_batch.mean(axis=1)
             std = X_batch.std(axis=1)
 
-            X_batch = (X_batch - mean.reshape(BATCH_SIZE,1))/std.reshape(BATCH_SIZE,1)
-            y_batch = (y_batch - mean.reshape(BATCH_SIZE,1))/std.reshape(BATCH_SIZE,1)
+            X_batch = (X_batch - mean.reshape(BATCH_SIZE,1))/std.reshape(BATCH_SIZE,1) + np.random.normal(0,0.5,X_batch.shape).astype(np.float32)
+            Y_batch = (Y_batch - mean.reshape(BATCH_SIZE,1))/std.reshape(BATCH_SIZE,1) + np.random.normal(0,0.5,Y_batch.shape).astype(np.float32) 
 
-           
-            loss = predict.train_on_batch(X_batch,y_batch)
-            test_loss = predict.test_on_batch(test_x,test_y)
-            print("epoch:%d batch:%d/%d loss:%f test_loss%f"%(epoch,index,num_batches,loss,test_loss))
-        predicted=predict.predict(X_batch)
-        time = [i for i in range(OUTPUT_SIZE)]
+            yl = model(X_batch)
+            loss = F.mean_squared_error(yl,Y_batch)
+
+            model.cleargrads()
+            loss.backward()
+            opt.update()
+
+            chainer.config.train = False
+            test_loss = F.mean_squared_error(model(test_x),test_y)
+            chainer.config.train = True
+            print("epoch:%d batch:%d/%d loss:%f test_loss:%f"%(epoch,index,num_batches,loss.data,test_loss.data))
             
-        plt.plot(time,predicted[0],label="predict",linestyle="--")
-        plt.plot(time,y_batch[0],label="real")
-        plt.show()
-            
-        predict.save_weights('predict.h5')
+        serializers.save_npz('predict.model', model)
 
 def predict_price(input_data,output_data=None):
-    predict=predict_model()
+    chainer.config.train = False
+    model = predict_model(INPUT_SIZE, OUTPUT_SIZE,32)
     try:
-        predict.load_weights('predict.h5')
+        serializers.load_npz("predict.model", model)
         print("loaded")
     except:
         print("couldn't load")
@@ -131,13 +128,14 @@ def predict_price(input_data,output_data=None):
     data = (data-mean)/std
     
     data=data.reshape(1,INPUT_SIZE)
-    predicted=predict.predict(data)
+    predicted=model(data).data
+    chainer.config.train = True
     
     y1=np.concatenate((y1, predicted.reshape(OUTPUT_SIZE,)*std+mean))
     y2=np.concatenate((y2, output_data))
-    
-    plt.plot(x,y1,label="predict",linestyle="--")
-    plt.plot(x,y2,label="real")
+    plt.plot(x,y1,label="predict")
+    plt.plot(x,y2,label="real",linestyle="--")
     plt.show()
-#train()
+
+train()
     
